c40cd90c5f071255b4ade9ade4eeccd95ac9c8e0
Sinmiloluwa <mofeoluwae@gmail.com>
2025-07-18 07:03:19 +0100

    dispatch event

--- app/Events/TodoItemCreated.php
+++ app/Events/TodoItemCreated.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class TodoItemCreated
+class TodoItemCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
